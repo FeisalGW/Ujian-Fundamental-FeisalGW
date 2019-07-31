75ffb9ebba04b25bd66df7d9a8d486351d093a8e
FeisalGW <53326056+FeisalGW@users.noreply.github.com>
2019-07-31 12:06:24 +0700

    Add files via upload

---
+++ ujian_fundamental_feisalGW.py
@@ -0,0 +1,41 @@
+# # ##nomer1
+def calculate_years(principal, interest, tax, desired):
+    for principal in p:
+        p=principal*years   
+
+    for interest in i:
+        i=interest*years
+        
+    for tax in t:
+        t=tax*years
+        
+    for desired in d:
+    
+    if ((p+i-t)<desired)
+
+    return(x)
+calculate_years()
+
+# ##nomer2
+a = int(input('Masukkan 2 angka pertama: '))
+b = int(input('Masukkan 2 angka kedua: '))
+ribuan_a = a // 10
+puluhan_a = a % 10
+ratusan_b = b // 10
+puluhan_b = b % 10
+
+print(str(ribuan_a) + '000'+str(ratusan_b)+'00'+str(puluhan_a)+'0'+str(puluhan_b)+'0' )
+
+    
+             
+
+##nomer3
+def tower_builder(n_floors, block_size):
+    w, h = block_size
+    z=''
+    for n_floors in range (w,h):
+        for block_size in range (w,h):
+            z+=' * '
+        z+= '\n'
+    return(z)
+tower_builder(3(2,1))
